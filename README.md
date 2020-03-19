ea2e99a1ef1a86c260a2e90a2ae5f4ea134df2c3
Faza Fahleraz <ffahleraz@gmail.com>
2020-03-20 06:13:33 +0700

    Add SAC training script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gym-warehouse
+
+Setup:
+```
+$ pip install pandas
+$ pip install tensorflow
+$ pip install tensorflow-probability
+$ pip install 'ray[debug]'
+$ pip install 'ray[rllib]'
+$ pip install box2d-py
+```
---
+++ train_sac.py
@@ -0,0 +1,51 @@
+import argparse
+
+import ray
+from ray.rllib.agents.sac.sac import SACTrainer
+from ray.tune.registry import register_env
+from ray.tune.logger import pretty_print
+
+from warehouse import Warehouse
+
+
+def main(save_dir: str, load_dir: str, num_iterations: int) -> None:
+    ray.init()
+
+    register_env("Warehouse-v0", lambda _: Warehouse())
+    trainer = SACTrainer(
+        env="Warehouse-v0",
+        config={
+            "Q_model": {"hidden_activation": "relu", "hidden_layer_sizes": (256, 256),},
+            "policy_model": {"hidden_activation": "relu", "hidden_layer_sizes": (256, 256),},
+            "normalize_actions": False,
+            "no_done_at_end": False,
+            "learning_starts": 20000,
+            "timesteps_per_iteration": 4000,
+        },
+    )
+    if load_dir is not None:
+        trainer.restore(load_dir)
+
+    for i in range(num_iterations):
+        print("==> Iteration Start")
+
+        result = trainer.train()
+        print(pretty_print(result))
+
+        if i % 20 == 0:
+            checkpoint = trainer.save(save_dir)
+            print("==> Checkpoint saved at", checkpoint)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--num_iterations", type=int, required=True, help="number of training iterations"
+    )
+    parser.add_argument(
+        "-s", "--save", type=str, required=True, help="path to the folder to save model"
+    )
+    parser.add_argument("-l", "--load", type=str, help="path to the folder to load model")
+    args = parser.parse_args()
+    print(args)
+    main(save_dir=args.save, load_dir=args.load, num_iterations=args.num_iterations)
