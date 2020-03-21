73e8a18493790763f80e5dbffefd2571c02f182a
Faza Fahleraz <ffahleraz@gmail.com>
2020-03-21 14:53:13 +0700

    Further reduce environment complexity

--- warehouse_test.py
+++ warehouse_test.py
@@ -4,12 +4,16 @@ import numpy as np
 
 from warehouse import Warehouse
 
+
+NUM_AGENTS: int = 2  # 4
+
+
 if __name__ == "__main__":
     env = Warehouse()
     observations = env.reset()
     done = False
     while not done:
-        action_dict = {f"{i}": np.array([-1.0, -1.0], dtype=np.float32) for i in range(4)}
+        action_dict = {f"{i}": np.array([-1.0, -1.0], dtype=np.float32) for i in range(NUM_AGENTS)}
 
         if observations["0"]["self_availability"][0] == 0:
             target = observations["0"]["self_delivery_target"][0:2]
