21a867ce0a8cf599504d0fa781d750419d7d41c4
Faza Fahleraz <ffahleraz@gmail.com>
2020-04-29 22:33:23 +0700

    Update experiment configs

--- warehouse/variants.py
+++ warehouse/variants.py
@@ -17,9 +17,9 @@ class Warehouse2(Warehouse):
     def __init__(self) -> None:
         super(Warehouse2, self).__init__(
             num_agents=2,
-            num_requests=2,
-            area_dimension=16,
-            pickup_racks_arrangement=[4, 8, 12],
+            num_requests=16,
+            area_dimension=20,
+            pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
             pickup_wait_duration=40,
         )
@@ -29,9 +29,9 @@ class Warehouse4(Warehouse):
     def __init__(self) -> None:
         super(Warehouse4, self).__init__(
             num_agents=4,
-            num_requests=4,
-            area_dimension=16,
-            pickup_racks_arrangement=[4, 8, 12],
+            num_requests=16,
+            area_dimension=20,
+            pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
             pickup_wait_duration=40,
         )
@@ -41,9 +41,9 @@ class Warehouse6(Warehouse):
     def __init__(self) -> None:
         super(Warehouse6, self).__init__(
             num_agents=6,
-            num_requests=6,
-            area_dimension=16,
-            pickup_racks_arrangement=[4, 8, 12],
+            num_requests=16,
+            area_dimension=20,
+            pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
             pickup_wait_duration=40,
         )
@@ -53,9 +53,9 @@ class Warehouse8(Warehouse):
     def __init__(self) -> None:
         super(Warehouse8, self).__init__(
             num_agents=8,
-            num_requests=8,
-            area_dimension=16,
-            pickup_racks_arrangement=[4, 8, 12],
+            num_requests=16,
+            area_dimension=20,
+            pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
             pickup_wait_duration=40,
         )
@@ -65,7 +65,7 @@ class Warehouse10(Warehouse):
     def __init__(self) -> None:
         super(Warehouse10, self).__init__(
             num_agents=10,
-            num_requests=10,
+            num_requests=16,
             area_dimension=20,
             pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
@@ -77,7 +77,7 @@ class Warehouse12(Warehouse):
     def __init__(self) -> None:
         super(Warehouse12, self).__init__(
             num_agents=12,
-            num_requests=12,
+            num_requests=16,
             area_dimension=20,
             pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
@@ -89,7 +89,7 @@ class Warehouse14(Warehouse):
     def __init__(self) -> None:
         super(Warehouse14, self).__init__(
             num_agents=14,
-            num_requests=14,
+            num_requests=16,
             area_dimension=20,
             pickup_racks_arrangement=[4, 8, 12, 16],
             episode_duration=400,
